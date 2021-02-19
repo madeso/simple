e217e9431d37d77cb866e758625d82f3ba19a6dd
Gustav <sir.gustav.the.coder@gmail.com>
2021-02-19 22:50:53 +0100

    meshpart to source and header

--- src/engine/meshpart.cc
+++ src/engine/meshpart.cc
@@ -1,13 +1,12 @@
-﻿#include <memory>
+﻿#include "engine/meshpart.h"
+
+#include <memory>
 #include <vector>
 
 #include "engine/animation.h"
 #include "engine/axisangle.h"
-#include "engine/material.h"
 #include "engine/medialoader.h"
-#include "engine/meshdef.h"
 #include "engine/opengl.h"
-#include "engine/poseable.h"
 #include "engine/pushedmatrix.h"
 #include "engine/texture.h"
 
@@ -15,61 +14,51 @@ namespace SimpleEngine
 {
     struct Poseable;
 
-    struct MeshPart
+    MeshPart::MeshPart(MediaLoader* ml, const MaterialDef& m, MeshDef* def, std::shared_ptr<Poseable> p)
+        : poseable(p)
     {
-        std::vector<std::vector<Vertex>> faces;
-        std::shared_ptr<Material> mat;
-        std::shared_ptr<Poseable> poseable;
-        int id = -1;
-
-        MeshPart(MediaLoader* ml, const MaterialDef& m, MeshDef* def, std::shared_ptr<Poseable> p)
-            : poseable(p)
+        mat = std::make_shared<Material>(m.diffuse, ml->fetch<Texture>(m.texture));
+        for (const auto& tri : def->TrianglesFor(m))
         {
-            mat = std::make_shared<Material>(m.diffuse, ml->fetch<Texture>(m.texture));
-            for (const auto& tri : def->TrianglesFor(m))
+            faces.emplace_back(def->lookup(tri));
+        }
+    }
+
+    int MeshPart::Id()
+    {
+        if (id == -1)
+            id = calculateId();
+        return id;
+    }
+
+    int MeshPart::calculateId() const
+    {
+        return 0;
+    }
+
+    void MeshPart::render(const vec3& p, const quat& rot) const
+    {
+        PushedMatrix dnum;
+
+        mat->apply();
+        glTranslatef(p.x, p.y, p.z);
+        AxisAngle aa = rot.AxisAngle();
+        // apply rotation
+        glRotatef(aa.angle.inDegrees(), aa.axis.x, aa.axis.y, aa.axis.z);
+        glBegin(GL_TRIANGLES);
+        for (auto& face : faces)
+        {
+            for (int i = 0; i < 3; ++i)
             {
-                faces.emplace_back(def->lookup(tri));
+                Vertex v = face[i];
+                vec3 vert = (poseable->CurrentPose() != nullptr)
+                                ? poseable->CurrentPose()->transforms[v.bone] * v.vertex
+                                : v.vertex;
+                glNormal3f(v.normal.x, v.normal.y, v.normal.z);
+                glTexCoord2f(v.uv.x, v.uv.y);
+                glVertex3f(vert.x, vert.y, vert.z);
             }
         }
-
-        int Id()
-        {
-            if (id == -1)
-                id = calculateId();
-            return id;
-        }
-
-        int calculateId() const
-        {
-            return 0;
-        }
-
-        void render(vec3 p, quat rot)
-        {
-            {
-                PushedMatrix dnum;
-
-                mat->apply();
-                glTranslatef(p.x, p.y, p.z);
-                AxisAngle aa = rot.AxisAngle();
-                // apply rotation
-                glRotatef(aa.angle.inDegrees(), aa.axis.x, aa.axis.y, aa.axis.z);
-                glBegin(GL_TRIANGLES);
-                for (auto& face : faces)
-                {
-                    for (int i = 0; i < 3; ++i)
-                    {
-                        Vertex v = face[i];
-                        vec3 vert = (poseable->CurrentPose() != nullptr)
-                                        ? poseable->CurrentPose()->transforms[v.bone] * v.vertex
-                                        : v.vertex;
-                        glNormal3f(v.normal.x, v.normal.y, v.normal.z);
-                        glTexCoord2f(v.uv.x, v.uv.y);
-                        glVertex3f(vert.x, vert.y, vert.z);
-                    }
-                }
-                glEnd();
-            }
-        }
-    };
+        glEnd();
+    }
 }
---
+++ src/engine/meshpart.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include <memory>
+#include <vector>
+
+#include "engine/material.h"
+#include "engine/meshdef.h"
+#include "engine/poseable.h"
+
+namespace SimpleEngine
+{
+    struct MediaLoader;
+    struct MaterialDef;
+    struct MeshDef;
+    struct vec3;
+    struct quat;
+
+    struct MeshPart
+    {
+        std::vector<std::vector<Vertex>> faces;
+        std::shared_ptr<Material> mat;
+        std::shared_ptr<Poseable> poseable;
+        int id = -1;
+
+        MeshPart(MediaLoader* ml, const MaterialDef& m, MeshDef* def, std::shared_ptr<Poseable> p);
+
+        int Id();
+
+        int calculateId() const;
+
+        void render(const vec3& p, const quat& rot) const;
+    };
+}
