9e0bf12a98b435c780c65c3716fc9de652073518
Gustav <sir.gustav.the.coder@gmail.com>
2021-02-19 23:29:23 +0100

    split compiledmesh into source and header

--- src/engine/compiledmesh.cc
+++ src/engine/compiledmesh.cc
@@ -1,61 +1,51 @@
-﻿#include <memory>
+﻿#include "engine/compiledmesh.h"
+
+#include <memory>
 #include <stdexcept>
 
-#include "engine/meshdef.h"
-#include "engine/meshpart.h"
-#include "engine/poseable.h"
 #include "engine/renderlist.h"
 
 namespace SimpleEngine
 {
-    struct CompiledPose;
-    struct MediaLoader;
-
-    struct CompiledMesh : public Poseable
+    CompiledMesh::CompiledMesh(MediaLoader* ml, MeshDef* def)
     {
-        std::vector<std::shared_ptr<MeshPart>> parts;
-        std::shared_ptr<CompiledPose> pose;
-
-        CompiledMesh(MediaLoader* ml, MeshDef* def)
+        std::vector<std::string> errors;
+        for (auto& m : def->Materials())
         {
-            std::vector<std::string> errors;
-            for (auto& m : def->Materials())
+            if (def->hasTrianglesFor(*m))
             {
-                if (def->hasTrianglesFor(*m))
+                try
                 {
-                    try
-                    {
-                        parts.emplace_back(std::make_shared<MeshPart>(ml, *m, def, this));
-                    }
-                    catch (const std::runtime_error& e)
-                    {
-                        errors.emplace_back(e.what());
-                    }
+                    parts.emplace_back(std::make_shared<MeshPart>(ml, *m, def, this));
+                }
+                catch (const std::runtime_error& e)
+                {
+                    errors.emplace_back(e.what());
                 }
             }
-
-            if (errors.empty() == false)
-            {
-                throw std::runtime_error("Several errors occured");
-            }
         }
 
-        void sendToRenderer(RenderList* r, const vec3& pos, const quat& rot)
+        if (errors.empty() == false)
         {
-            for (auto& part : parts)
-            {
-                r->add(part, pos, rot);
-            }
+            throw std::runtime_error("Several errors occured");
         }
+    }
 
-        void setPose(std::shared_ptr<CompiledPose> new_pose)
+    void CompiledMesh::sendToRenderer(RenderList* r, const vec3& pos, const quat& rot)
+    {
+        for (auto& part : parts)
         {
-            pose = new_pose;
+            r->add(part, pos, rot);
         }
+    }
 
-        std::shared_ptr<CompiledPose> CurrentPose() override
-        {
-            return pose;
-        }
-    };
+    void CompiledMesh::setPose(std::shared_ptr<CompiledPose> new_pose)
+    {
+        pose = new_pose;
+    }
+
+    std::shared_ptr<CompiledPose> CompiledMesh::CurrentPose()
+    {
+        return pose;
+    }
 }
---
+++ src/engine/compiledmesh.h
@@ -0,0 +1,27 @@
+﻿#include <memory>
+
+#include "engine/animation.h"
+#include "engine/meshpart.h"
+#include "engine/poseable.h"
+
+namespace SimpleEngine
+{
+    struct MediaLoader;
+    struct RenderList;
+    struct vec3;
+    struct quat;
+
+    struct CompiledMesh : public Poseable
+    {
+        std::vector<std::shared_ptr<MeshPart>> parts;
+        std::shared_ptr<CompiledPose> pose;
+
+        CompiledMesh(MediaLoader* ml, MeshDef* def);
+
+        void sendToRenderer(RenderList* r, const vec3& pos, const quat& rot);
+
+        void setPose(std::shared_ptr<CompiledPose> new_pose);
+
+        std::shared_ptr<CompiledPose> CurrentPose() override;
+    };
+}
