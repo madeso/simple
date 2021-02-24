f50bb2b9aa5ea71413b9ebb4ee1bd6e1972fc941
Gustav <sir.gustav.the.coder@gmail.com>
2021-02-24 19:24:40 +0100

    removed using namespace from header

--- examples/demo/demo.h
+++ examples/demo/demo.h
@@ -11,37 +11,35 @@
 #include "engine/window.h"
 #include "engine/world.h"
 
-using namespace SimpleEngine;
-
 namespace SimpleTest
 {
-    struct Demo : public Window
+    struct Demo : public SimpleEngine::Window
     {
-        Camera cam;
+        SimpleEngine::Camera cam;
 
         bool running = true;
 
-        vec2 mousesmooth = vec2(0, 0);
-        vec3 movement = vec3(0, 0, 0);
+        SimpleEngine::vec2 mousesmooth = SimpleEngine::vec2(0, 0);
+        SimpleEngine::vec3 movement = SimpleEngine::vec3(0, 0, 0);
 
         float sensitivity = 0.1f;
         float mousesmoothing = 6;
 
-        std::shared_ptr<Key> rightleft;
-        std::shared_ptr<Key> forback;
-        std::shared_ptr<Key> updown;
-        std::shared_ptr<Hold> sprint;
+        std::shared_ptr<SimpleEngine::Key> rightleft;
+        std::shared_ptr<SimpleEngine::Key> forback;
+        std::shared_ptr<SimpleEngine::Key> updown;
+        std::shared_ptr<SimpleEngine::Hold> sprint;
 
-        std::shared_ptr<World> world;
-        std::shared_ptr<fse::Pipeline> pipe;
+        std::shared_ptr<SimpleEngine::World> world;
+        std::shared_ptr<SimpleEngine::fse::Pipeline> pipe;
 
         Demo();
 
         void Render();
 
-        void OnFrame(float delta, const vec2& mouse_movement);
+        void OnFrame(float delta, const SimpleEngine::vec2& mouse_movement);
 
-        void OnButton(const Ckey& button, bool down);
+        void OnButton(const SimpleEngine::Ckey& button, bool down);
 
         void OnBeginRender();
     };
