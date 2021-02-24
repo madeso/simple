50489bcae48d9f165acbe4fe69b2a2da75523f60
Gustav <sir.gustav.the.coder@gmail.com>
2021-02-24 19:22:26 +0100

    started splitting demo source and header

--- examples/demo/demo.cc
+++ examples/demo/demo.cc
@@ -1,107 +1,81 @@
-﻿#include <SDL2/SDL.h>
+﻿#include "demo/demo.h"
 
-#include <memory>
-#include <vector>
+#include <SDL2/SDL.h>
 
-#include "engine/camera.h"
-#include "engine/ckey.h"
 #include "engine/filesystem.h"
-#include "engine/fse/pipeline.h"
-#include "engine/key.h"
 #include "engine/medialoader.h"
 #include "engine/opengl.h"
 #include "engine/quat.h"
 #include "engine/rect.h"
 #include "engine/setup.h"
-#include "engine/vec2.h"
-#include "engine/vec3.h"
-#include "engine/window.h"
-#include "engine/world.h"
 
 using namespace SimpleEngine;
 
 namespace SimpleTest
 {
-    struct Demo : public Window
+    Demo::Demo()
+        : rightleft(
+              std::make_shared<PlusMinus>(std::make_shared<Hold>(Ckey(SDLK_d)), std::make_shared<Hold>(Ckey(SDLK_a))))
+        , forback(
+              std::make_shared<PlusMinus>(std::make_shared<Hold>(Ckey(SDLK_w)), std::make_shared<Hold>(Ckey(SDLK_s))))
+        , updown(std::make_shared<PlusMinus>(std::make_shared<Hold>(Ckey(SDLK_SPACE)),
+                                             std::make_shared<Hold>(Ckey(SDLK_LCTRL))))
+        , sprint(std::make_shared<Hold>(Ckey(SDLK_LSHIFT)))
     {
-        Camera cam = Camera();
+        Setup::basicOpenGL();
 
-        bool running = true;
+        auto fs = std::make_shared<FileSystem>();
+        fs->addDefaultRoots("pretty good", "simple test");
+        auto loader = MediaLoader(fs);
+        // Texture sample = loader.fetch<Texture>("sample.bmp");
 
-        vec2 mousesmooth = vec2(0, 0);
-        vec3 movement = vec3(0, 0, 0);
+        world = World::Load(&loader, "level01.lvl");
+        // world.add(MeshInstance(loader.fetch<Mesh>("basicroad.obj")));
 
-        float sensitivity = 0.1f;
-        float mousesmoothing = 6;
+        pipe = fse::Pipeline::Create("pipeline.xml", &loader, Width, Height);
+    }
 
-        std::shared_ptr<Key> rightleft =
-            std::make_shared<PlusMinus>(std::make_shared<Hold>(Ckey(SDLK_d)), std::make_shared<Hold>(Ckey(SDLK_a)));
-        std::shared_ptr<Key> forback =
-            std::make_shared<PlusMinus>(std::make_shared<Hold>(Ckey(SDLK_w)), std::make_shared<Hold>(Ckey(SDLK_s)));
-        std::shared_ptr<Key> updown = std::make_shared<PlusMinus>(std::make_shared<Hold>(Ckey(SDLK_SPACE)),
-                                                                  std::make_shared<Hold>(Ckey(SDLK_LCTRL)));
-        std::shared_ptr<Hold> sprint = std::make_shared<Hold>(Ckey(SDLK_LSHIFT));
-
-        std::shared_ptr<World> world;
-        std::shared_ptr<fse::Pipeline> pipe;
-
-        Demo()
-        {
-            Setup::basicOpenGL();
-
-            auto fs = std::make_shared<FileSystem>();
-            fs->addDefaultRoots("pretty good", "simple test");
-            auto loader = MediaLoader(fs);
-            // Texture sample = loader.fetch<Texture>("sample.bmp");
-
-            world = World::Load(&loader, "level01.lvl");
-            // world.add(MeshInstance(loader.fetch<Mesh>("basicroad.obj")));
-
-            pipe = fse::Pipeline::Create("pipeline.xml", &loader, Width, Height);
-        }
-
-        void Render()
-        {
-            /*fbo.updateTexture(delegate()
-                    {
-                        world.render(Width, Height, cam);
-                    });*/
-            auto ra = fse::RenderArgs(world, cam, Width, Height);
-            pipe->render(&ra);
-            /*Shader.Bind(shader);
-                    FullscreenQuad.render(fbo, Width, Height);
-                    Shader.Unbind();*/
-        }
-
-        void OnFrame(float delta, const vec2& mouse_movement)
-        {
-            /*frame.begin();
+    void Demo::Render()
+    {
+        /*fbo.updateTexture(delegate()
+                {
                     world.render(Width, Height, cam);
-                    frame.swap();*/
-            mousesmooth = vec2::Curve(mouse_movement, mousesmooth, mousesmoothing);
-            movement = vec3::Curve(Key::Combine(rightleft, updown, forback).Normalized() * (3 + sprint->Value() * 3),
-                                   movement, 5);
+                });*/
+        auto ra = fse::RenderArgs(world, cam, Width, Height);
+        pipe->render(&ra);
+        /*Shader.Bind(shader);
+                FullscreenQuad.render(fbo, Width, Height);
+                Shader.Unbind();*/
+    }
 
-            // math::Quaternion(math::op::vec3::yAxisPositive, -x) * math::Quaternion(mRotation.getRight(), y) *
-            // mRotation;
+    void Demo::OnFrame(float delta, const vec2& mouse_movement)
+    {
+        /*frame.begin();
+                world.render(Width, Height, cam);
+                frame.swap();*/
+        mousesmooth = vec2::Curve(mouse_movement, mousesmooth, mousesmoothing);
+        movement =
+            vec3::Curve(Key::Combine(rightleft, updown, forback).Normalized() * (3 + sprint->Value() * 3), movement, 5);
 
-            quat final = quat::FpsQuat(cam.rotation, mousesmooth.x * sensitivity, mousesmooth.y * sensitivity);
-            cam.location = cam.location + cam.rotation.getRUI(movement) * delta;
-            cam.rotate(final);
-        }
+        // math::Quaternion(math::op::vec3::yAxisPositive, -x) * math::Quaternion(mRotation.getRight(), y) *
+        // mRotation;
 
-        void OnButton(const Ckey& button, bool down)
-        {
-            if (button == Ckey(SDLK_ESCAPE))
-                running = false;
-            else
-                Key::Run(button, down, {rightleft, forback, updown, sprint});
-        }
+        quat final = quat::FpsQuat(cam.rotation, mousesmooth.x * sensitivity, mousesmooth.y * sensitivity);
+        cam.location = cam.location + cam.rotation.getRUI(movement) * delta;
+        cam.rotate(final);
+    }
 
-        void OnBeginRender()
-        {
-            Setup::Viewport(Rect::FromLTWH(0, 0, Width, Height));
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        }
-    };
+    void Demo::OnButton(const Ckey& button, bool down)
+    {
+        if (button == Ckey(SDLK_ESCAPE))
+            running = false;
+        else
+            Key::Run(button, down, {rightleft, forback, updown, sprint});
+    }
+
+    void Demo::OnBeginRender()
+    {
+        Setup::Viewport(Rect::FromLTWH(0, 0, Width, Height));
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
 }
---
+++ examples/demo/demo.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <memory>
+
+#include "engine/camera.h"
+#include "engine/ckey.h"
+#include "engine/fse/pipeline.h"
+#include "engine/key.h"
+#include "engine/vec2.h"
+#include "engine/vec3.h"
+#include "engine/window.h"
+#include "engine/world.h"
+
+using namespace SimpleEngine;
+
+namespace SimpleTest
+{
+    struct Demo : public Window
+    {
+        Camera cam;
+
+        bool running = true;
+
+        vec2 mousesmooth = vec2(0, 0);
+        vec3 movement = vec3(0, 0, 0);
+
+        float sensitivity = 0.1f;
+        float mousesmoothing = 6;
+
+        std::shared_ptr<Key> rightleft;
+        std::shared_ptr<Key> forback;
+        std::shared_ptr<Key> updown;
+        std::shared_ptr<Hold> sprint;
+
+        std::shared_ptr<World> world;
+        std::shared_ptr<fse::Pipeline> pipe;
+
+        Demo();
+
+        void Render();
+
+        void OnFrame(float delta, const vec2& mouse_movement);
+
+        void OnButton(const Ckey& button, bool down);
+
+        void OnBeginRender();
+    };
+}
